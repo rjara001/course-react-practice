8a1f1a85e12872f814d15463ee7356d0f24a0859
rodrigo.jara <rodrigo.jara@andbank.com>
2023-04-25 01:01:54 +0200

    first commit

---
+++ src/pages/using-jsx.js
@@ -0,0 +1,11 @@
+// Create a simple JSX element that renders a header with the title "My Portfolio".
+
+export const UsingJSX = () => {
+
+    return <div>
+
+        <h1>My Portfolio</h1>
+
+        <h3>This is a JSX Page that just renders a headers</h3>
+    </div>
+}
